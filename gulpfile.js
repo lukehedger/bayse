a9ca48a8f0a4b23da928e05110bfda724315cd23
Luke Hedger <luke.hedger@gmail.com>
2014-07-01 16:32:48 +0100

    Updated Boilerplate files and added PageSpeed Insights Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,22 @@ var gulp = require('gulp'),
 	myth = require('gulp-myth'),
 	minifycss = require('gulp-minify-css'),
 	gutil = require('gulp-util'),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	psi = require('psi');
 
+// config
+var config = {
+	paths: {
+		coffee: 'coffee/**/*.coffee',
+		myth: 'myth/**/*.css'
+	},
+	site: 'https://github.com/lukehedger/bayse', // use public URL of your site
+	key: '' // use Google Developer API Key if you have one
+};
+
+// CoffeeScript
 gulp.task('coffee', function () {
-	gulp.src('coffee/**/*.coffee')
+	gulp.src(config.paths.coffee)
 		.pipe(sourcemaps.init())
 		.pipe(coffee().on('error', gutil.log))
 		.pipe(sourcemaps.write('./maps'))
@@ -15,8 +27,9 @@ gulp.task('coffee', function () {
 		.pipe(browserSync.reload({stream:true, once: true}));
 });
 
+// Myth
 gulp.task('myth', function () {
-	gulp.src('myth/**/*.css')
+	gulp.src(config.paths.myth)
 		.pipe(myth())
 		.pipe(gulp.dest('css'))
 		.pipe(minifycss({
@@ -43,13 +56,33 @@ gulp.task('browser-sync', function() {
     });
 });
 
+// PageSpeed Insights
+gulp.task('pagespeed.mobile', function (cb) {
+	psi({
+	    // key: config.key
+	    nokey: 'true',
+	    url: config.site,
+	    strategy: 'mobile',
+	}, cb);
+});
+gulp.task('pagespeed.desktop', function (cb) {
+	psi({
+	    // key: config.key,
+	    nokey: 'true',
+	    url: config.site,
+	    strategy: 'desktop',
+	}, cb);
+});
 
+// bundled tasks
 gulp.task('default', function () {
 	gulp.start('coffee', 'myth');
 });
 
 gulp.task('watch', function() {
 	gulp.start('browser-sync');
-	gulp.watch('myth/**/*.css', ['myth']);
-	gulp.watch('coffee/**/*.coffee', ['coffee']);
+	gulp.watch(config.paths.myth, ['myth']);
+	gulp.watch(config.paths.coffee, ['coffee']);
 });
+
+gulp.task('analyse', ['pagespeed.desktop', 'pagespeed.mobile']);
